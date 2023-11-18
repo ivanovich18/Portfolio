f884456fdcff57d8df719198cf3cf78a9f9a9b94
ivanovich18 <ivan.suralta@ctu.edu.ph>
2023-11-18 16:55:31 +0800

    Update mobile menu icon in Nav component

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -4,6 +4,7 @@ import logo from '../../assets/favicon_io/android-chrome-192x192.png';
 import { Link } from 'react-scroll';
 import { RiArrowRightSLine } from "react-icons/ri";
 import { GiHamburgerMenu } from "react-icons/gi";
+import { RxHamburgerMenu } from "react-icons/rx";
 
 
 function Nav() {
@@ -34,16 +35,16 @@ function Nav() {
                 <button className='desktopMenuBtn'>Contact Me <RiArrowRightSLine className='rightArrow' /></button>
             </Link>
 
-            <GiHamburgerMenu className='mobileMenu' onClick={() => setShowMenu(!showMenu)} />
+            <RxHamburgerMenu className='mobileMenu' onClick={() => setShowMenu(!showMenu)} />
 
             <div className='navMenu' style={{ display: showMenu ? 'flex' : 'none' }}>
                 <Link activeClass='active' to='hero' spy={true} smooth={true} offset={-80} duration={500} className='mobileItem' onClick={() => setShowMenu(false)}>Home</Link>
                 <Link activeClass='active' to='about' spy={true} smooth={true} offset={-65} duration={500} className='mobileItem' onClick={() => setShowMenu(false)}>About</Link>
                 <Link activeClass='active' to='skills' spy={true} smooth={true} offset={-50} duration={500} className='mobileItem' onClick={() => setShowMenu(false)}>Skills</Link>
                 <Link activeClass='active' to='experience' spy={true} smooth={true} offset={-80} duration={500} className='mobileItem' onClick={() => setShowMenu(false)}>Experience</Link>
-                <Link activeClass='active' to='projects' spy={true} smooth={true} offset={-80} duration={500} className='mobileItem' onClick={() => setShowMenu(false)}>Projects</Link>
+                <Link activeClass='active' to='projects' spy={true} smooth={true} offset={-60} duration={500} className='mobileItem' onClick={() => setShowMenu(false)}>Projects</Link>
                 <Link activeClass='active' to='certifications' spy={true} smooth={true} offset={-80} duration={500} className='mobileItem' onClick={() => setShowMenu(false)}>Certifications</Link>
-                <Link activeClass='active' to='contact' spy={true} smooth={true} offset={-90} duration={500} className='mobileItem' onClick={() => setShowMenu(false)}>Contact Me</Link>
+                <Link activeClass='active' to='contact' spy={true} smooth={true} offset={0} duration={500} className='mobileItem' onClick={() => setShowMenu(false)}>Contact Me</Link>
             </div>
         </nav >
     );
