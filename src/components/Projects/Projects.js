297aaeb393b54fd555a1bc541dd670414c3c3988
ivanovich18 <ivan.suralta@ctu.edu.ph>
2023-11-19 23:37:32 +0800

    Add AOS animation to Projects component

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,34 +1,46 @@
+import React, { useEffect } from 'react';
 import quiztionAIPic from '../../assets/others/quiz-tion.png';
 import studentEnrollmentPic from '../../assets/others/student-enrollment.png';
 import littleLemonPic from '../../assets/others/little-lemon.png';
 import mangataAndGalloPic from '../../assets/others/mangata-and-gallo.png';
 import '../Projects/projects.css';
 
+import AOS from "aos";
+import 'aos/dist/aos.css';
+
 function Projects() {
+
+    useEffect(() => {
+        AOS.init({
+            once: true,
+            duration: 2000
+        })
+    }, [])
+
     return (
         <section id='projects' className='projects'>
-            <h2 className='header2'>Projects</h2>
+            <h2 className='header2' data-aos="fade-down" data-aos-duration="600" data-aos-delay="300">Projects</h2>
             <div className='projectShowcase'>
                 <a className='transform' href='https://github.com/ivanovich18/Quiz-tion-AI' target='_blank'>
-                    <section className='projectPic'>
+                    <section className='projectPic' data-aos="zoom-in" data-aos-duration="600" data-aos-delay="500">
                         <h3>Quiz-tion AI</h3>
                         <img src={quiztionAIPic} alt='Quiztion AI Website' />
                     </section>
                 </a>
                 <a href='https://github.com/ivanovich18/Student-Enrollment-Application' target='_blank'>
-                    <section className='projectPic'>
+                    <section className='projectPic' data-aos="zoom-in" data-aos-duration="600" data-aos-delay="700">
                         <h3>Student Enrollment Application</h3>
                         <img src={studentEnrollmentPic} alt='Student Enrollment Application' />
                     </section>
                 </a>
                 <a href='https://github.com/ivanovich18/little-lemon-ivan-suralta' target='_blank'>
-                    <section className='projectPic'>
+                    <section className='projectPic' data-aos="zoom-in" data-aos-duration="600" data-aos-delay="900">
                         <h3>Little Lemon Restaurant Reservation</h3>
                         <img src={littleLemonPic} alt='Little Lemon Restaurant Reservation Website' />
                     </section>
                 </a>
                 <a href='https://github.com/ivanovich18/Mangata-and-Gallo-Landing-Page' target='_blank'>
-                    <section className='projectPic'>
+                    <section className='projectPic' data-aos="zoom-in" data-aos-duration="600" data-aos-delay="1100">
                         <h3>Mangata & Gallo E-commerce Page</h3>
                         <img src={mangataAndGalloPic} alt='Mangata and Gallo E-commerce Webpage' />
                     </section>
