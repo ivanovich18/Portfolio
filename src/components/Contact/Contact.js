84b132aa48086ea0c2d2f82503865ae9682a577f
ivanovich18 <ivan.suralta@ctu.edu.ph>
2023-11-27 21:29:23 +0800

    Refactor contact form and add form validation

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,51 +1,100 @@
-import React, { useRef, useEffect } from 'react';
-import '../Contact/contact.css'
+import React, { useRef, useEffect, useState } from 'react';
+import '../Contact/contact.css';
 import emailjs from '@emailjs/browser';
 
-import AOS from "aos";
+import AOS from 'aos';
 import 'aos/dist/aos.css';
 
 function Contact() {
+  useEffect(() => {
+    AOS.init({
+      once: true,
+      duration: 2000,
+    });
+  }, []);
 
-    useEffect(() => {
-        AOS.init({
-            once: true,
-            duration: 2000
-        })
-    }, [])
+  const form = useRef();
 
-    const form = useRef();
+  const [isFormValid, setIsFormValid] = useState(false);
 
-    const sendEmail = (e) => {
-        e.preventDefault();
+  const sendEmail = (e) => {
+    e.preventDefault();
 
-        emailjs.sendForm('service_s81q0il', 'template_hgp5xsr', form.current, 'GXYsuqagPT2dk0x13')
-            .then((result) => {
-                console.log(result.text);
-                alert('Email sent successfully!');
-                e.target.reset();
-            }, (error) => {
-                console.log(error.text);
-                alert('Please fill all fields');
-            });
-    };
+    emailjs
+      .sendForm('service_s81q0il', 'template_hgp5xsr', form.current, 'GXYsuqagPT2dk0x13')
+      .then(
+        (result) => {
+          console.log(result.text);
+          alert('Email sent successfully!');
+          e.target.reset();
+        },
+        (error) => {
+          console.log(error.text);
+          alert('Please fill all fields');
+        }
+      );
+  };
 
-    return (
-        <section id='contact' className='contact'>
-            <h2 className='header2 smallerHeader' data-aos="fade-down" data-aos-duration="600" data-aos-delay="300">Contact Me</h2>
-            <p data-aos="zoom-in" data-aos-duration="600" data-aos-delay="500">I am always free for collaboration.</p>
-            <p data-aos="zoom-in" data-aos-duration="600" data-aos-delay="600">Whether you need help for projects or other things, send me a message and let's chat.</p>
-            <form className='contactForm' ref={form} onSubmit={sendEmail}>
-                <input required type='text' className='name' placeholder='Your Name' name='from_name' data-aos="flip-left" data-aos-duration="600" data-aos-delay="700" />
-                <input required type='email' className='email' placeholder='Your Email' name='from_email' data-aos="flip-left" data-aos-duration="600" data-aos-delay="800" />
-                <textarea required className='message' data-aos="flip-left" data-aos-duration="600" data-aos-delay="900" name='message' rows='5' placeholder='Your Message' ></textarea>
-                <div>
-                    <button type='submit' value='Send' className='submitBtn' data-aos="zoom-in" data-aos-duration="600" data-aos-delay="1000">Submit</button>
-                </div>
-            </form>
+  const handleInputChange = () => {
+    const nameInput = form.current['from_name'];
+    const emailInput = form.current['from_email'];
+    const messageInput = form.current['message'];
 
-        </section>
-    );
+    const isValid = nameInput.value.trim() !== '' && emailInput.value.trim() !== '' && messageInput.value.trim() !== '';
+    setIsFormValid(isValid);
+  };
+
+  return (
+    <section id="contact" className="contact">
+      <h2 className="header2 smallerHeader" data-aos="fade-down" data-aos-duration="600" data-aos-delay="300">
+        Contact Me
+      </h2>
+      <p data-aos="zoom-in" data-aos-duration="600" data-aos-delay="500">I am always free for collaboration.</p>
+      <p data-aos="zoom-in" data-aos-duration="600" data-aos-delay="600">
+        Whether you need help for projects or other things, send me a message and let's chat.
+      </p>
+      <form className="contactForm" ref={form} onSubmit={sendEmail}>
+        <input
+          required
+          type="text"
+          className="name"
+          placeholder="Your Name"
+          name="from_name"
+          data-aos="flip-left"
+          data-aos-duration="600"
+          data-aos-delay="700"
+          onChange={handleInputChange}
+        />
+        <input
+          required
+          type="email"
+          className="email"
+          placeholder="Your Email"
+          name="from_email"
+          data-aos="flip-left"
+          data-aos-duration="600"
+          data-aos-delay="800"
+          onChange={handleInputChange}
+        />
+        <textarea
+          required
+          className="message"
+          data-aos="flip-left"
+          data-aos-duration="600"
+          data-aos-delay="900"
+          name="message"
+          rows="5"
+          placeholder="Your Message"
+          onChange={handleInputChange}
+        ></textarea>
+        <div>
+          <button type="submit" value="Send" className="submitBtn" data-aos="zoom-in" data-aos-duration="600" data-aos-delay="1000" disabled={!isFormValid}>
+            Submit
+          </button>
+        </div>
+      </form>
+    </section>
+  );
 }
 
 export default Contact;
