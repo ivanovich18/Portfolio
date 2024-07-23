fc712596f051b31e5e252c29d414c4bfeb73b378
ivanovich18 <ivan.suralta@ctu.edu.ph>
2024-07-23 11:58:01 +0800

    Changed the aboutImage and updated some personal details

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,6 +1,6 @@
 // import aboutMePic from '../../assets/aboutme-pic.png';
 import React, { useEffect } from 'react';
-import aboutMePic2 from '../../assets/others/aboutme-pic2.png';
+import aboutMePic3 from '../../assets/others/aboutme-pic4.jpg';
 import metaBadge from '../../assets/others/meta-front-end-developer-certificate.png';
 import gdscBadge from '../../assets/others/gdsc-badge.png';
 import '../About/about.css'
@@ -23,9 +23,8 @@ function About() {
         <main id='about' className='aboutPage'>
             <section className='aboutLeft'>
                 <div data-aos="zoom-in-down" data-aos-duration="600" data-aos-delay="300">
-                <img className='aboutImage' src={aboutMePic2} alt='Ivan portrait photo' />
+                <img className='aboutImage' src={aboutMePic3} alt='Ivan portrait photo' />
                 </div>
-                
 
                 <div className='badges'>
                     <a href='https://www.credly.com/badges/8b5468c3-69ae-43d5-8d48-d43d7335e526/public_url' target='_blank'  data-aos="zoom-in-right" data-aos-duration="600" data-aos-delay="500">
@@ -39,11 +38,13 @@ function About() {
             <section className='aboutRight'>
                 <p className='text1' data-aos="fade-left" data-aos-duration="600" data-aos-delay="1100">👋🏻 Hello! My name is</p>
                 <h2 className='text2' data-aos="fade-left" data-aos-duration="600" data-aos-delay="1300">IVAN SURALTA</h2>
-                <p className='text3' data-aos="fade-left" data-aos-duration="600" data-aos-delay="1500">A 3rd year Computer Engineering student at Cebu Technological University - Main Campus, highly interested in UI/UX Design and frontend web development, with specialization in ReactJS.</p>
+                <p className='text3' data-aos="fade-left" data-aos-duration="600" data-aos-delay="1500">A 4th year Computer Engineering student at Cebu Technological University - Main Campus. I'm interested in UI/UX design, web development, and project management.</p>
 
-                <p className='text3' data-aos="fade-left" data-aos-duration="600" data-aos-delay="1700">I am also the Lead of Google Developer Student Clubs - Cebu Technological University (GDSC CTU) for the term 2023-2024.</p>
+                <p className='text3' data-aos="fade-left" data-aos-duration="600" data-aos-delay="1700">I was the Lead of the Google Developer Student Clubs - CTU in 2023-2024 and currently serve as a Consultant.</p>
 
-                <p className='text3' data-aos="fade-left" data-aos-duration="600" data-aos-delay="1900">As an aspiring full-stack web developer, I am constantly expanding my skillset to ensure I can contribute to all aspects of web development.</p>
+                <p className='text3' data-aos="fade-left" data-aos-duration="600" data-aos-delay="1900">I'm also a Galaxy Campus Student Ambassador representing Cebu Technological University for Samsung Philippines.</p>
+
+                <p className='text3' data-aos="fade-left" data-aos-duration="600" data-aos-delay="2100">As an aspiring seasoned software engineer, I am constantly learning and growing to contribute effectively across the entire development process.</p>
 
                 <div className='buttons'>
                     <a href='https://www.linkedin.com/in/ivan-suralta/' target='_blank' data-aos="zoom-out-up" data-aos-duration="600" data-aos-delay="2100">
